a85e980a5dbcb0d9725eca335e7fb703a5e14c8a
Shivani131 <85801670+Shivani131@users.noreply.github.com>
2023-01-03 15:33:54 +0530

    Add files via upload

---
+++ Inner Classes/Test.java
@@ -0,0 +1,15 @@
+interface Showable{
+  void show();
+  interface Message{
+   void msg();
+  }
+}
+
+class Test implements Showable.Message{
+ public void msg(){System.out.println("Hello nested interface");}
+
+ public static void main(String args[]){
+  Showable.Message message=new Test();//upcasting here
+  message.msg();
+ }
+}
